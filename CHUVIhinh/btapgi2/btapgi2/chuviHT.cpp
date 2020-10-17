2b3c24b516bd743146686e009059c4f9e9a0a537
Administrator <Administrator@P5_28>
2020-10-17 16:18:58 +0700

    chuvi

---
+++ CHUVIhinh/btapgi2/btapgi2/chuviHT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int r;
+	cout << "nhap ban kinh hinh tron: ";
+	cin >> r;
+	int pi = 3.14;
+	int cvHT;
+	cvHT = 2*r* pi ;
+	cout << "chu vi HT la: " << cvHT;
+
+
+
+	system("pause");
+
+}
