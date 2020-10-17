8b3d6a717c9b87aa74af7ce06930cfa23d0be700
Administrator <Administrator@P5_28>
2020-10-17 15:59:55 +0700

    congran dum

---
+++ congrandum/btapgi2/btapgi2/chuviHCN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int dai, rong;
+	cout << "nhap chieu dai HCN: ";
+	cin >> dai;
+	cout << "nhap chieu rong HCN: ";
+	cin >> rong;
+
+	int cvHCN;
+	cvHCN = (dai + rong) * 2;
+	cout <<"chu vi HCN la: "<< cvHCN;
+
+	
+	
+	system("pause");
+	
+}
---
+++ congrandum/btapgi2/btapgi2/cvHV.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	int r;
+	cout << "nhap cach HV: ";
+	cin >> r;
+
+	int cvHV;
+	cvHV = 4 * r;
+
+	cout << "chu vi HV la: " << cvHV << endl;
+
+
+
+	system("pause");
+
+}
